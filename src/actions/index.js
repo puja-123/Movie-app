047a53ad16e243ff5d7fbadcc975696c45c2c910
puja-123 <bhard.puja@gmail.com>
2021-04-17 22:30:23 +0530

    Changing the state to add favourite

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+// {
+//     type : 'ADD_MOVIES',
+//     movies : [m1,m2,m3]
+// }
+// {
+//     type : 'DECREASE_COUNT'
+// }
+
+//action type
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+export function addMovies(movies) {
+    return {
+        type : ADD_MOVIES,
+        movies
+    }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-function App() {
+import {data} from '../data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
+import {addMovies} from '../actions'; 
+class App extends React.Component {
+  componentDidMount(){
+    const {store} = this.props;
+    store.subscribe(() => {
+      console.log('UPDATED');
+      this.forceUpdate();
+    });
+    //make api call
+    //dispatch action
+    store.dispatch(addMovies(data));
+
+    console.log('STATE', this.props.store.getState());
+  }
+  render(){
+  const { list } = this.props.store.getState(); //{list: [], favourites : []}
+  console.log("RENDER",this.props.store.getState());
   return (
     <div className="App">
-      project setup
+      <Navbar />
+      <div className = "main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+        <div className="list">
+          {list.map((movie,index) => (
+            <MovieCard movie = {movie} key={`movie-${index}`}/>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
-
+}
 export default App;
   
